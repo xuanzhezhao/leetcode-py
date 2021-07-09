7a58ee632cac0013454e7f6e1c7a48e01158a646
xuanzhezhao <xz2920@ic.ac.uk>
2021-07-09 07:44:58 +0100

    add sorted_squares

---
+++ array/sorted_squares.py
@@ -0,0 +1,24 @@
+from typing import List
+"""Leetcode Question 977: https://leetcode-cn.com/problems/squares-of-a-sorted-array/"""
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        i = 0
+        j = len(nums)-1
+        k = j
+        result = [0] * len(nums)
+        while i<=j:
+            if nums[i]*nums[i]<nums[j]*nums[j]:
+                result[k] = nums[j]*nums[j]
+                j = j-1
+            else:
+                result[k] = nums[i]*nums[i]
+                i = i+1
+            k = k - 1
+
+        return result
+
+if __name__ == '__main__':
+    solution = Solution()
+    #assert solution.search([2, 7, 11, 15], 15) == 3
+    print(solution.sortedSquares([-7, -2, 1,7,11, 15]))
